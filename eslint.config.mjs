32a466bf1ddb9d7752a6156a7fef6d23447fd071
solux <lee.yeji0327@gmail.com>
2025-09-02 22:56:34 +0900

    del: eslint-plugin-tailwindcss import문 삭제

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,6 @@ import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
 import prettier from 'eslint-plugin-prettier'
-import tailwindcss from 'eslint-plugin-tailwindcss'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
