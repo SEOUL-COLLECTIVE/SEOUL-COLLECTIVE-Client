b5f734a16de0f5dc19f4d4c204911f4cc07a90d5
solux <lee.yeji0327@gmail.com>
2025-09-06 23:39:48 +0900

    refactor: onClick -> Link

--- src/components/Cards/ShoppingCard.tsx
+++ src/components/Cards/ShoppingCard.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
-type ContentsCardProps = {
+type ShoppingCardProps = {
   imageUrl: string
   brand: string
   product: string
@@ -19,28 +19,27 @@ export default function ShoppingCard({
   price,
   store,
   link,
-}: ContentsCardProps) {
-  const router = useRouter()
-  const onClickCard = (link: string) => {
-    router.push(link)
-  }
+}: ShoppingCardProps) {
   return (
-    <div
+    <Link
+      rel="noopener noreferrer nofollow"
       className="flex h-full w-full cursor-pointer flex-col font-semibold gap-y-2"
-      onClick={() => onClickCard(link)}
+      href={link}
+      target="_blank"
+      aria-label={`${brand} ${product} - ${price} @ ${store}`}
     >
       {/* 이미지 영역 */}
       <div className="relative w-full aspect-[187/254]">
-        <Image src={imageUrl} alt="thumbnail" fill className="object-cover" />
+        <Image src={imageUrl} alt={`${brand} ${product}`} fill className="object-cover" />
       </div>
 
       {/* 텍스트 영역 */}
-      <div className="flex flex-col gap-1 text-[12px] font-semibold">
+      <div className="flex flex-col gap-1 text-[12px]">
         <div className="">{brand}</div>
         <div className="line-clamp-1 whitespace-pre-line break-words">{product}</div>
         <div className="">{price}</div>
         <div className="">{store}</div>
       </div>
-    </div>
+    </Link>
   )
 }
