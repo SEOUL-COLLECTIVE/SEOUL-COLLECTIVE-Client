865d6c5935e30194c022e4e84417af3bbff70f70
solux <lee.yeji0327@gmail.com>
2025-09-09 02:53:42 +0900

    fix: categorypageprops 관련 build 오류 해결

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -2,14 +2,10 @@ import { notFound } from 'next/navigation'
 import { navItems } from '@/data/navItem'
 
 interface CategoryPageProps {
-  params: {
-    category: string
-  }
+  category: string
 }
 
-export default function CategoryPage({ params }: CategoryPageProps) {
-  const { category } = params
-
+export default function CategoryPage({ category }: CategoryPageProps) {
   // 카테고리 이름을 대문자로 변환하여 navItems에서 찾기
   const categoryName = category.toUpperCase()
   const navItem = navItems.find((item) => item.name === categoryName)
