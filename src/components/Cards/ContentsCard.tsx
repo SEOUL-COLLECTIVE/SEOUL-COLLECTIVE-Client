a39e89bb2a9b1115a3e7c63239a7404374d63b92
solux <lee.yeji0327@gmail.com>
2025-09-09 01:30:22 +0900

    fix: sectionStyle 다시 파일 내부로 이동

--- src/components/Cards/ContentsCard.tsx
+++ src/components/Cards/ContentsCard.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Image from 'next/image'
-import { sectionType } from '@/data/sectionStyle'
+import { SectionStyle } from '@/types/card'
 
 type ContentsCardProps = {
   imageUrl: string
@@ -9,6 +9,69 @@ type ContentsCardProps = {
   title: string
   section: string
 }
+
+const sectionType: SectionStyle[] = [
+  {
+    section: 'main',
+    font: {
+      category: 'text-p13',
+      title: 'text-p28',
+    },
+    gap: {
+      image_categroy: 'pt-6',
+      category_title: 'pt-3',
+    },
+    image_ratio: 'aspect-[770/582]',
+  },
+  {
+    section: 'sub',
+    font: {
+      category: 'text-p11',
+      title: 'text-p20',
+    },
+    gap: {
+      image_categroy: 'pt-3',
+      category_title: 'pt-1',
+    },
+    image_ratio: 'aspect-[334/235]',
+  },
+  {
+    section: 'testDrive',
+    font: {
+      category: 'text-p10',
+      title: 'text-p16',
+    },
+    gap: {
+      image_categroy: 'pt-2',
+      category_title: 'pt-1',
+    },
+    image_ratio: 'aspect-[371/290]',
+  },
+  {
+    section: 'shopping',
+    font: {
+      category: 'text-p13',
+      title: 'text-p26',
+    },
+    gap: {
+      image_categroy: 'pt-3',
+      category_title: 'pt-1',
+    },
+    image_ratio: 'aspect-[514/508]',
+  },
+  {
+    section: 'latest',
+    font: {
+      category: 'text-p10',
+      title: 'text-p16',
+    },
+    gap: {
+      image_categroy: 'pt-4',
+      category_title: 'pt-1',
+    },
+    image_ratio: 'aspect-[340/255]',
+  },
+]
 
 export default function ContentsCard({ imageUrl, category, title, section }: ContentsCardProps) {
   const style = sectionType.find((s) => s.section === section)
