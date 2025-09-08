605e0a3c745d478b26220fb7e4fbb01c1ae92bc0
solux <lee.yeji0327@gmail.com>
2025-09-08 19:15:48 +0900

    refactor: section별 card 더미데이터 & 타입 분리

--- src/components/Sections/Latest.tsx
+++ src/components/Sections/Latest.tsx
@@ -1,59 +1,7 @@
 'use client'
 
 import ContentsCard from '../Cards/ContentsCard'
-
-interface LatestItem {
-  id: number
-  section: string
-  title: string
-  category: string
-  imageUrl: string
-}
-
-const latestData: LatestItem[] = [
-  {
-    id: 1,
-    section: 'testDrive',
-    title: '8 Best Mineral Body Sunscreens\nfor Gentle All-Over Protection',
-    category: 'SUNSCREEN',
-    imageUrl: '/test/thumbnail_01.jpg',
-  },
-  {
-    id: 2,
-    section: 'testDrive',
-    title: '9 Best Melasma Concealers to\nEven Out Your Skin Tone in Seconds',
-    category: 'MAKEUP',
-    imageUrl: '/test/thumbnail_01.jpg',
-  },
-  {
-    id: 3,
-    section: 'testDrive',
-    title: 'We Tried SkinCeuticals C E Ferulic\n— And We Have Thoughts',
-    category: 'SKIN',
-    imageUrl: '/test/thumbnail_01.jpg',
-  },
-  {
-    id: 4,
-    section: 'testDrive',
-    title: '8 Best Mineral Body Sunscreens\nfor Gentle All-Over Protection',
-    category: 'SUNSCREEN',
-    imageUrl: '/test/thumbnail_01.jpg',
-  },
-  {
-    id: 5,
-    section: 'testDrive',
-    title: '9 Best Melasma Concealers to\nEven Out Your Skin Tone in Seconds',
-    category: 'MAKEUP',
-    imageUrl: '/test/thumbnail_01.jpg',
-  },
-  {
-    id: 6,
-    section: 'testDrive',
-    title: 'We Tried SkinCeuticals C E Ferulic\n— And We Have Thoughts',
-    category: 'SKIN',
-    imageUrl: '/test/thumbnail_01.jpg',
-  },
-]
+import { latestData } from '@/data/sectionData'
 
 export default function Latest() {
   return (
---
+++ src/types/card.ts
@@ -0,0 +1,8 @@
+// section 공용 card type
+export type CardTypes = {
+  id: number
+  imageUrl: string
+  category: string
+  title: string
+  section: string
+}
