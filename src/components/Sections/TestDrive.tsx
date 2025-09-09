5e1f5122003eb9e638d5286e93e7afbb8618d230
solux <lee.yeji0327@gmail.com>
2025-09-09 16:53:44 +0900

    chore: test drive 폰트 크기 및 weight 변경

--- src/components/Sections/TestDrive.tsx
+++ src/components/Sections/TestDrive.tsx
@@ -7,8 +7,8 @@ export default function TestDrive() {
   return (
     <div className="bg-yellow py-[3.543rem] -mx-[5.375rem]">
       <div className="text-center px-[3.375rem] flex flex-col">
-        <div className="font-futura-bold text-p26">TEST DRIVE</div>
-        <div className="font-futura text-p16 pb-10">{`Browse SC-approved beauty products that we've put to the test ourselves`}</div>
+        <div className="font-futura-bold text-p50">TEST DRIVE</div>
+        <div className="font-bold text-p17 pb-10">{`Browse SC-approved beauty products that we've put to the test ourselves`}</div>
         <div className="grid grid-cols-3 text-start gap-x-10">
           {testDriveData.map((item) => (
             <ContentsCard
