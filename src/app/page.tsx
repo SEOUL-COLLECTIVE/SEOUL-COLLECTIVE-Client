59986e4842e0c3ac2fb3ab9c9e35a6007f60999e
solux <lee.yeji0327@gmail.com>
2025-10-10 01:53:52 +0900

    add: 랜딩페이지에서 useArticles로 전체 아티클 호출

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,18 @@ import TestDrive from '@/components/Sections/TestDrive'
 import SCShopping from '@/components/Sections/SCShopping'
 import Latest from '@/components/Sections/Latest'
 import CategoryPreview from '@/components/Sections/CategoryPreview'
+import { useArticles } from '@/hooks/use-contentful'
 
 export default function Page() {
   const [username, setUsername] = useState<string>('Emma')
+
+  // 🔥 여기서 딱 1번만 전체 아티클 가져오기!
+  const { data: articles, isLoading } = useArticles()
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div>
       <div className="pb-3 text-p20 font-semibold">Hi, {username}</div>
