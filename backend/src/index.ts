22d179182e596d1b258d89b01f3e7f43e2540d93
Yeonjung Tae <june.yeonjung@gmail.com>
2025-11-06 16:53:42 +0900

    백엔드 CORS 보안 수정 및 비밀번호 정규화 수정

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,12 +9,19 @@ import authRoutes from './routes/authRouter';
 dotenv.config()
 
 const app: Express = express()
+
 const DEV_HOST = process.env.DEV_HOST || '127.0.0.1';
 const PORT = Number(process.env.PORT) || 5000;
 const API_VERSION = process.env.API_VERSION || '/api/v1';
 
+const FRONTEND_DEV_HOST = process.env.FRONTEND_DEV_HOST || 'localhost';
+const FRONTEND_DEV_PORT = process.env.FRONTEND_DEV_PORT || 3000;
+
 // 미들웨어 설정
-app.use(cors({ origin: '*' })) // CORS 허용
+app.use(cors({ 
+  origin: `http://${FRONTEND_DEV_HOST}:${FRONTEND_DEV_PORT}`, 
+  credentials: true 
+})) // CORS 허용
 app.use(express.json()) // Request body를 JSON으로 파싱
 
 // Swagger 문서 경로
